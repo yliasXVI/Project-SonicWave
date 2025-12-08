24288a3871344963c38f090efe94370ab84c0ddb
Ylias <yliaskok@gmail.com>
2025-12-08 21:04:22 +0100

    ddddd

--- obj/Debug/net9.0-windows/ProjectSonicWave.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/ProjectSonicWave.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSonicWave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01883a586ea4afa168c90fcada9303504d180482")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c82e554dd2fbb3909351b5e3db2b829f2ac95d47")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSonicWave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSonicWave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
